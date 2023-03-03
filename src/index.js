03bc69e9ca2af24395fb55c36dd3ecc857ed3492
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2023-03-03 21:32:27 +0200

    Added some static details about client

--- src/index.js
+++ src/index.js
@@ -14,8 +14,11 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
     let user = socket.conn.id
+    let count = socket.conn.server.clientsCount
+    let userAddress = socket.conn.remoteAddress
     socket.emit('welcomeMessage', `Your unique ID is - ${user}`)
-    socket.broadcast.emit('welcomeMessage', `UserID ${user} has joined!`)
+    socket.broadcast.emit('message', `UserID ${user} has joined! IP: ${userAddress}`)
+    socket.broadcast.emit('message', `Online users - ${count}`)
 
     socket.on('sendMessage', (message, callback) => {
         io.emit('message', `[${user}]: ${message}`);
@@ -24,6 +27,7 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         io.emit('message', `User_${user} has left!`);
+        io.emit('message', `Online users - ${--count}`)
     })
 })
 
